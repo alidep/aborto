7e8c689776e97f51890abbbb1356737e77ed4110
alidep <amdepsky@gmail.com>
2022-02-20 14:37:13 -0800

    Add files via upload

---
+++ data/translate.js
@@ -0,0 +1,513 @@
+var translate = {
+    "AGUILAR, EDUARDO ALBERTO": {
+        "nombre": "AGUILAR, Eduardo Alberto",
+        "img": "imgs/actuales/aguilar_eduardo_alberto.png",
+        "bloque": "Justicialista-Frente para la Victoria",
+        "distrito": "Chaco",
+        "es_actual": true
+    },
+    "ALMIR\u00d3N, ANA CLAUDIA": {
+        "nombre": "ALMIRON, Ana Claudia",
+        "img": "imgs/actuales/almiron_ana_claudia.png",
+        "bloque": "Justicialista-Frente para la Victoria",
+        "distrito": "Corrientes",
+        "es_actual": true
+    },
+    "ALPEROVICH, JOS\u00c9 JORGE": {
+        "nombre": "ALPEROVICH, José",
+        "img": "imgs/actuales/alperovich_jose.png",
+        "bloque": "Justicialista-Frente para la Victoria",
+        "distrito": "Tucumán",
+        "es_actual": true
+    },
+    "BASUALDO, ROBERTO GUSTAVO": {
+        "nombre": "BASUALDO, Roberto Gustavo",
+        "img": "imgs/actuales/basualdo_roberto_gustavo.png",
+        "bloque": "Producción y Trabajo",
+        "distrito": "San Juan",
+        "es_actual": true
+    },
+    "BLAS, IN\u00c9S IMELDA": {
+        "nombre": "BLAS, Inés Imelda",
+        "img": "imgs/actuales/blas_ines_imelda.png",
+        "bloque": "Justicialista-Frente para la Victoria",
+        "distrito": "Catamarca",
+        "es_actual": true
+    },
+    "BOYADJIAN, MIRIAM RUTH": {
+        "nombre": "BOYADJIAN, Miriam Ruth",
+        "img": "imgs/actuales/boyadjian_miriam_ruth.png",
+        "bloque": "Movimiento Popular Fueguino",
+        "distrito": "Tierra del Fuego",
+        "es_actual": true
+    },
+    "BRAILLARD POCCARD, N\u00c9STOR PEDRO": {
+        "nombre": "BRAILLARD POCCARD, Néstor",
+        "img": "imgs/actuales/braillard_poccard_nestor.png",
+        "bloque": "Frente Pro",
+        "distrito": "Corrientes",
+        "es_actual": true
+    },
+    "BRIZUELA Y DORIA DE CARA, OLGA INES": {
+        "nombre": "BRIZUELA Y DORIA DE CARA, Olga Ines",
+        "img": "imgs/actuales/brizuela_y_doria_de_cara_olga_ines.png",
+        "bloque": "",
+        "distrito": "La Rioja",
+        "es_actual": true
+    },
+    "BULLRICH, ESTEBAN JOSE": {
+        "nombre": "BULLRICH, Esteban Jose",
+        "img": "imgs/actuales/bullrich_esteban_jose.png",
+        "bloque": "",
+        "distrito": "Buenos Aires",
+        "es_actual": true
+    },
+    "CASERIO, CARLOS ALBERTO": {
+        "nombre": "CASERIO, Carlos Alberto",
+        "img": "imgs/actuales/caserio_carlos_alberto.png",
+        "bloque": "Unión por Córdoba",
+        "distrito": "Córdoba",
+        "es_actual": true
+    },
+    "CASTILLO, OSCAR AN\u00cdBAL": {
+        "nombre": "CASTILLO, Oscar Anibal 	",
+        "img": "imgs/actuales/castillo_oscar_anibal.png",
+        "bloque": "Frente Cívico y Social de Catamarca",
+        "distrito": "Catamarca",
+        "es_actual": true
+    },
+    "CATAL\u00c1N MAGNI, JULIO C\u00c9SAR": {
+        "nombre": "CATALAN MAGNI, Julio Cesar",
+        "img": "imgs/actuales/catalan_magni_julio_cesar.png",
+        "bloque": "Justicialista-Frente para la Victoria",
+        "distrito": "Tierra del Fuego",
+        "es_actual": true
+    },
+    "CATALFAMO, MARIA EUGENIA": {
+        "nombre": "CATALFAMO, Maria Eugenia",
+        "img": "imgs/actuales/catalfamo_maria_eugenia.png",
+        "bloque": "",
+        "distrito": "San Luis",
+        "es_actual": true
+    },
+    "CLOSS, MAURICE FABIAN": {
+        "nombre": "CLOSS, Maurice Fabián",
+        "img": "imgs/actuales/closs_maurice_fabian.png",
+        "bloque": "Justicialista",
+        "distrito": "Misiones",
+        "es_actual": true
+    },
+    "COBOS, JULIO C\u00c9SAR CLETO": {
+        "nombre": "COBOS, Julio Cobos Cleto",
+        "img": "imgs/actuales/cobos_julio_cesar_cleto.png",
+        "bloque": "",
+        "distrito": "Mendoza",
+        "es_actual": true
+    },
+    "COSTA, EDUARDO RAUL": {
+        "nombre": "COBOS, Julio César Cleto",
+        "img": "imgs/actuales/cobos_julio_cesar_cleto.png",
+        "bloque": "Unión Cívica Radical",
+        "distrito": "Mendoza",
+        "es_actual": true
+    },
+    "CREXELL, CARMEN LUCILA": {
+        "nombre": "CREXELL, Carmen Lucila",
+        "img": "imgs/actuales/crexell_carmen_lucila.png",
+        "bloque": "Movimiento Popular Neuquino",
+        "distrito": "Neuquén",
+        "es_actual": true
+    },
+    "DE ANGELI, ALFREDO LUIS": {
+        "nombre": "DE ANGELI, Alfredo Luis",
+        "img": "imgs/actuales/de_angeli_alfredo_luis.png",
+        "bloque": "Pro y Unión por Entre Ríos",
+        "distrito": "Entre Ríos",
+        "es_actual": true
+    },
+    "DURANGO, NORMA HAYD\u00c9E": {
+        "nombre": "DURANGO, Norma Haydee",
+        "img": "imgs/actuales/durango_norma_haydee.png",
+        "bloque": "",
+        "distrito": "La Pampa",
+        "es_actual": true
+    },
+    "EL\u00cdAS DE PEREZ, SILVIA BEATRIZ": {
+        "nombre": "DURANGO, Norma Haydée",
+        "img": "imgs/actuales/durango_norma_haydee.png",
+        "bloque": "Justicialista La Pampa",
+        "distrito": "La Pampa",
+        "es_actual": true
+    },
+    "ESP\u00cdNOLA, CARLOS MAURICIO": {
+        "nombre": "ESPINOLA, Carlos Mauricio",
+        "img": "imgs/actuales/espinola_carlos_mauricio.png",
+        "bloque": "Justicialista-Frente para la Victoria",
+        "distrito": "Corrientes",
+        "es_actual": true
+    },
+    "FERN\u00c1NDEZ DE KIRCHNER, CRISTINA E.": {
+        "nombre": "FERNANDEZ DE KIRCHNER, Cristina",
+        "img": "imgs/actuales/fernandez_de_kirchner_cristina.png",
+        "bloque": "Justicialista",
+        "distrito": "Buenos Aires",
+        "es_actual": true
+    },
+    "FERN\u00c1NDEZ SAGASTI, ANABEL": {
+        "nombre": "FERNÁNDEZ SAGASTI, Anabe",
+        "img": "imgs/actuales/fernandez_sagasti_anabel.png",
+        "bloque": "Justicialista-Frente para la Victoria",
+        "distrito": "Mendoza",
+        "es_actual": true
+    },
+    "FIAD, MARIO RAYMUNDO": {
+        "nombre": "FIAD, Mario Raymundo",
+        "img": "imgs/actuales/fiad_mario_raymundo.png",
+        "bloque": "",
+        "distrito": "Jujuy",
+        "es_actual": true
+    },
+    "FIORE VI\u00d1UALES, MAR\u00cdA CRISTINA DEL VALLE": {
+        "nombre": "FIORE VINUALES, Maria Cristina del Valle",
+        "img": "imgs/actuales/fiore_vinuales_maria_cristina_del_valle.png",
+        "bloque": "Justicialista-Frente para la Victoria",
+        "distrito": "Salta",
+        "es_actual": true
+    },
+    "FUENTES, MARCELO JORGE": {
+        "nombre": "FUENTES, Marcelo Jorge",
+        "img": "imgs/actuales/fuentes_marcelo_jorge.png",
+        "bloque": "Justicialista-Frente para la Victoria",
+        "distrito": "Neuquén",
+        "es_actual": true
+    },
+    "GARC\u00cdA LARRABURU, SILVINA MARCELA": {
+        "nombre": "GARCIA LARRABURU, Silvia Marcela",
+        "img": "imgs/historicos/garcia_larraburu_silvia_marcela.png",
+        "bloque": "Justicialista-Frente para la Victoria",
+        "distrito": "Río Negro",
+        "es_actual": true
+    },
+    "GIACOPPO, SILVIA DEL ROSARIO": {
+        "nombre": "GIACOPPO, Silvia del Rosario",
+        "img": "imgs/actuales/giacoppo_silvia_del_rosario.png",
+        "bloque": "Union Civica Radical",
+        "distrito": "Jujuy",
+        "es_actual": true
+    },
+    "GONZALEZ, GLADYS ESTHER": {
+        "nombre": "GONZALEZ, Gladys Esther",
+        "img": "imgs/actuales/gonzalez_gladys_esther.png",
+        "bloque": "",
+        "distrito": "Buenos Aires",
+        "es_actual": true
+    },
+    "GONZ\u00c1LEZ, MAR\u00cdA TERESA MARGARITA": {
+        "nombre": "GONZALEZ, María Teresa Margarita",
+        "img": "imgs/actuales/gonzalez_maria_teresa_margarita.png",
+        "bloque": "Justicialista-Frente para la Victoria",
+        "distrito": "Formosa",
+        "es_actual": true
+    },
+    "GONZ\u00c1LEZ, NANCY SUSANA": {
+        "nombre": "Gonzales, Nancy Susana",
+        "img": "imgs/actuales/gonzalez_nancy_susana.png",
+        "bloque": "",
+        "distrito": "Chubut",
+        "es_actual": true
+    },
+    "GUASTAVINO, PEDRO GUILLERMO \u00c1NGEL": {
+        "nombre": "GUASTAVINO, Pedro Guillermo Ángel",
+        "img": "imgs/actuales/guastavino_pedro_guillermo_angel.png",
+        "bloque": "Justicialista-Frente para la Victoria",
+        "distrito": "Entre Ríos",
+        "es_actual": true
+    },
+    "IANNI, ANA MARIA": {
+        "nombre": "IANNI, Ana Maria",
+        "img": "imgs/actuales/ianni_ana_maria.png",
+        "bloque": "",
+        "distrito": "Santa Cruz",
+        "es_actual": true
+    },
+    "IT\u00daRREZ DE CAPPELLINI, ADA ROSA DEL VALLE": {
+        "nombre": "ITURREZ de CAPELLINI, Ada Rosa",
+        "img": "imgs/actuales/iturrez_de_capellini_ada_rosa.png",
+        "bloque": "Frente Cívico por Santiago",
+        "distrito": "Santiago del Estero",
+        "es_actual": true
+    },
+    "KUNATH, SIGRID ELISABETH": {
+        "nombre": "KUNATH, Sigrid Elisabeth",
+        "img": "imgs/actuales/kunath_sigrid_elisabeth.png",
+        "bloque": "Justicialista-Frente para la Victoria",
+        "distrito": "Entre Ríos",
+        "es_actual": true
+    },
+    "LOPEZ VALVERDE, CRISTINA DEL CARMEN": {
+        "nombre": "LOPEZ VALVERDE, Cristina Del Carmen",
+        "img": "imgs/actuales/lopez_valverde_cristina_del_carmen.png",
+        "bloque": "",
+        "distrito": "San Juan",
+        "es_actual": true
+    },
+    "LOVERA, DANIEL AN\u00cdBAL": {
+        "nombre": "LOVERA, Daniel Aníbal",
+        "img": "imgs/actuales/lovera_daniel_anibal.png",
+        "bloque": "Justicialista La Pampa",
+        "distrito": "La Pampa",
+        "es_actual": true
+    },
+    "LUENZO, ALFREDO H\u00c9CTOR": {
+        "nombre": "LUENZO, Alfredo Héctor",
+        "img": "imgs/actuales/luenzo_alfredo_hector.png",
+        "bloque": "Chubut Somos Todos",
+        "distrito": "Chubut",
+        "es_actual": true
+    },
+    "MARINO, JUAN CARLOS": {
+        "nombre": "MARINO, Juan Carlos",
+        "img": "imgs/actuales/marino_juan_carlos.png",
+        "bloque": "Unión Cívica Radical",
+        "distrito": "La Pampa",
+        "es_actual": true
+    },
+    "MART\u00cdNEZ, ERNESTO F\u00c9LIX": {
+        "nombre": "MARTÍNEZ, Ernesto Félix",
+        "img": "imgs/actuales/martinez_ernesto_felix.png",
+        "bloque": "Frente Pro",
+        "distrito": "Córdoba",
+        "es_actual": true
+    },
+    "MARTINEZ, JULIO CESAR": {
+        "nombre": "MARTINEZ, Julio Cesar",
+        "img": "imgs/actuales/martinez_julio_cesar.png",
+        "bloque": "",
+        "distrito": "La Rioja",
+        "es_actual": true
+    },
+    "MAYANS, JOS\u00c9 MIGUEL \u00c1NGEL": {
+        "nombre": "MAYANS, José Miguel Ángel",
+        "img": "imgs/actuales/mayans_jose_miguel_angel.png",
+        "bloque": "Justicialista",
+        "distrito": "Formosa",
+        "es_actual": true
+    },
+    "MENEM, CARLOS SA\u00daL": {
+        "nombre": "MENEM, Carlos Saúl",
+        "img": "imgs/actuales/menem_carlos_saul.png",
+        "bloque": "Federalismo y Liberación",
+        "distrito": "La Rioja",
+        "es_actual": true
+    },
+    "MERA, DALMACIO": {
+        "nombre": "MERA, Dalmacio Enrique",
+        "img": "imgs/actuales/mera_dalmacio_enrique.png",
+        "bloque": "Justicialista-Frente para la Victoria",
+        "distrito": "Catamarca",
+        "es_actual": true
+    },
+    "MIRKIN, BEATRIZ GRACIELA": {
+        "nombre": "MIRKIN, Beatriz Graciela",
+        "img": "imgs/actuales/mirkin_beatriz_graciela.png",
+        "bloque": "Justicialista-Frente para la Victoria",
+        "distrito": "Tucumán",
+        "es_actual": true
+    },
+    "MONTENEGRO, GERARDO ANTENOR": {
+        "nombre": "MONTENEGRO, Gerardo Antenor",
+        "img": "imgs/actuales/montenegro_gerardo_antenor.png",
+        "bloque": "Frente Popular",
+        "distrito": "Santiago del Estero",
+        "es_actual": true
+    },
+    "ODARDA, MAR\u00cdA MAGDALENA": {
+        "nombre": "ODARDA, María Magdalena",
+        "img": "imgs/actuales/odarda_maria_magdalena.png",
+        "bloque": "Frente Progresista-CC-ARI",
+        "distrito": "Río Negro",
+        "es_actual": true
+    },
+    "OJEDA, JOS\u00c9 ANATOLIO": {
+        "nombre": "OJEDA, José Anatolio",
+        "img": "imgs/actuales/ojeda_jose_anatolio.png",
+        "bloque": "Justicialista-Frente para la Victoria",
+        "distrito": "Tierra del Fuego",
+        "es_actual": true
+    },
+    "PAIS, JUAN MARIO": {
+        "nombre": "PAIS, Juan Mario",
+        "img": "imgs/actuales/pais_juan_mario.png",
+        "bloque": "Justicialista-Frente para la Victoria",
+        "distrito": "Chubut",
+        "es_actual": true
+    },
+    "PEREYRA, GUILLERMO JUAN": {
+        "nombre": "PEREYRA, Guillermo Juan",
+        "img": "imgs/actuales/pereyra_guillermo_juan.png",
+        "bloque": "Movimiento Popular Neuquino",
+        "distrito": "Neuquén",
+        "es_actual": true
+    },
+    "PEROTTI, OMAR \u00c1NGEL": {
+        "nombre": "PEROTTI, Omar Ángel",
+        "img": "imgs/actuales/perotti_omar_angel.png",
+        "bloque": "Justicialista-Frente para la Victoria",
+        "distrito": "Santa Fe",
+        "es_actual": true
+    },
+    "PETCOFF NAIDENOFF, LUIS CARLOS": {
+        "nombre": "PETCOFF NAIDENOFF, Luis Carlos",
+        "img": "imgs/actuales/petcoff_naidenoff_luis_carlos.png",
+        "bloque": "Unión Cívica Radical",
+        "distrito": "Formosa",
+        "es_actual": true
+    },
+    "PICHETTO, MIGUEL \u00c1NGEL": {
+        "nombre": "PICHETTO, Miguel Ángel",
+        "img": "imgs/actuales/pichetto_miguel_angel.png",
+        "bloque": "Justicialista",
+        "distrito": "Río Negro",
+        "es_actual": true
+    },
+    "PILATTI VERGARA, MAR\u00cdA IN\u00c9S": {
+        "nombre": "PILATTI VERGARA, Maria Ines Patricia",
+        "img": "imgs/actuales/pilatti_vergara_maria_ines_patricia.png",
+        "bloque": "Justicialista-Frente para la Victoria",
+        "distrito": "Chaco",
+        "es_actual": true
+    },
+    "PINEDO, FEDERICO": {
+        "nombre": "PINEDO, Federico",
+        "img": "imgs/actuales/pinedo_federico.png",
+        "bloque": "Frente Pro",
+        "distrito": "Ciudad Autónoma de Buenos Aires",
+        "es_actual": true
+    },
+    "POGGI, CLAUDIO JAVIER": {
+        "nombre": "POGGI, Claudio Javier",
+        "img": "imgs/actuales/poggi_claudio_javier.png",
+        "bloque": "",
+        "distrito": "San Luis",
+        "es_actual": true
+    },
+    "PORCEL DE RICCOBELLI, BLANCA": {
+        "nombre": "PORCEL DE RICCOBELLI, Blanca",
+        "img": "imgs/actuales/porcel_de_riccobelli_blanca.png",
+        "bloque": "",
+        "distrito": "Santiago del Estero",
+        "es_actual": true
+    },
+    "REUTEMANN, CARLOS ALBERTO": {
+        "nombre": "REUTEMANN, Carlos Alberto",
+        "img": "imgs/actuales/reutemann_carlos_alberto.png",
+        "bloque": "Justicialista",
+        "distrito": "Santa Fe",
+        "es_actual": true
+    },
+    "RODR\u00cdGUEZ MACHADO, LAURA ELENA": {
+        "nombre": "RODRÍGUEZ MACHADO, Laura Elena",
+        "img": "imgs/actuales/rodriguez_machado_laura_elena.png",
+        "bloque": "Frente Pro",
+        "distrito": "Córdoba",
+        "es_actual": true
+    },
+    "RODR\u00cdGUEZ SA\u00c1, ADOLFO": {
+        "nombre": "RODRIGUEZ SAA, Adolfo",
+        "img": "imgs/actuales/rodriguez_saa_adolfo.png",
+        "bloque": "Justicialista San Luis",
+        "distrito": "San Luis",
+        "es_actual": true
+    },
+    "ROMERO, JUAN CARLOS": {
+        "nombre": "ROMERO, Juan Carlos",
+        "img": "imgs/actuales/romero_juan_carlos.png",
+        "bloque": "Justicialista 8 de Octubre",
+        "distrito": "Salta",
+        "es_actual": true
+    },
+    "ROZAS, \u00c1NGEL": {
+        "nombre": "ROZAS, Angel",
+        "img": "imgs/actuales/rozas_angel.png",
+        "bloque": "Union Civica Radical",
+        "distrito": "Chaco",
+        "es_actual": true
+    },
+    "SACNUN, MAR\u00cdA DE LOS \u00c1NGELES": {
+        "nombre": "SACNUN, María de los Ángeles",
+        "img": "imgs/actuales/sacnun_maria_de_los_angeles.png",
+        "bloque": "Justicialista-Frente para la Victoria",
+        "distrito": "Santa Fe",
+        "es_actual": true
+    },
+    "SCHIAVONI, HUMBERTO LUIS ARTURO": {
+        "nombre": "SCHIAVONI, Humberto Luis Arturo",
+        "img": "imgs/actuales/schiavoni_humberto_luis_arturo.png",
+        "bloque": "",
+        "distrito": "Misiones",
+        "es_actual": true
+    },
+    "SNOPEK, GUILLERMO EUGENIO MARIO": {
+        "nombre": "SNOPEK, Guillermo Eugenio Mario",
+        "img": "imgs/actuales/snopek_guillermo_eugenio_mario.png",
+        "bloque": "",
+        "distrito": "Jujuy",
+        "es_actual": true
+    },
+    "SOLANAS, FERNANDO EZEQUIEL": {
+        "nombre": "SOLANAS, Fernando Ezequiel",
+        "img": "imgs/actuales/solanas_fernando_ezequiel.png",
+        "bloque": "Proyecto Sur-UNEN",
+        "distrito": "Ciudad Autónoma de Buenos Aires",
+        "es_actual": true
+    },
+    "SOLARI QUINTANA, MAGDALENA": {
+        "nombre": "SOLARI QUINTANA, Magdalena",
+        "img": "imgs/actuales/solari_quintana_magdalena.png",
+        "bloque": "",
+        "distrito": "Misiones",
+        "es_actual": true
+    },
+    "TAPIA, MARIA BELEN": {
+        "nombre": "TAPIA, Maria Belen",
+        "img": "imgs/actuales/tapia_maria_belen.png",
+        "bloque": "",
+        "distrito": "Santa Cruz",
+        "es_actual": true
+    },
+    "U\u00d1AC, JOSE RUBEN": {
+        "nombre": "Unac, Jose Ruben",
+        "img": "imgs/actuales/unac_jose_ruben.png",
+        "bloque": "",
+        "distrito": "San Juan",
+        "es_actual": true
+    },
+    "URTUBEY, RODOLFO JULIO": {
+        "nombre": "URTUBEY, Rodolfo Julio",
+        "img": "imgs/actuales/urtubey_rodolfo_julio.png",
+        "bloque": "Justicialista-Frente para la Victoria",
+        "distrito": "Salta",
+        "es_actual": true
+    },
+    "VARELA, MARTA": {
+        "nombre": "VARELA, Marta",
+        "img": "imgs/actuales/varela_marta.png",
+        "bloque": "Frente Pro",
+        "distrito": "Ciudad Autónoma de Buenos Aires",
+        "es_actual": true
+    },
+    "VERASAY, PAMELA FERNANDA": {
+        "nombre": "VERASAY, Pamela Fernanda",
+        "img": "imgs/actuales/verasay_pamela_fernanda.png",
+        "bloque": "Unión Cívica Radical",
+        "distrito": "Mendoza",
+        "es_actual": true
+    },
+    "MICHETTI, MARTA GABRIELA": {
+        "nombre": "MICHETTI, Marta Gabriela",
+        "img": "imgs/historicos/michetti_marta_gabriela.png",
+        "bloque": "Pro y Unión por Entre Ríos",
+        "distrito": "Ciudad Autónoma de Buenos Aires",
+        "es_actual": true
+    }
+}
